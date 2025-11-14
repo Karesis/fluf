bfdb4d8a96485ce668d8fda9af38fbf74f82f759
Karesis <yangyifeng23@mails.ucas.ac.cn>
2025-11-15 01:48:45 +0800

    fix(dbg): fix no `,` in fmt ... functions (add __VA_OPT__(, ))

--- include/core/msg/dbg.h
+++ include/core/msg/dbg.h
@@ -1,19 +1,18 @@
 /*
  *    Copyright 2025 Karesis
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-
 
 #pragma once
 
@@ -33,7 +32,7 @@
 #define dbg(fmt, ...)                                                          \
   do {                                                                         \
     fprintf(stderr, "[DEBUG] [%s:%d] %s(): " fmt "\n", __FILE__, __LINE__,     \
-            __func__, __VA_ARGS__);                                            \
+            __func__ __VA_OPT__(, ) __VA_ARGS__);                              \
   } while (0)
 
 #endif
