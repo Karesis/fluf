ffbb7fed89998421ec06642b6b48913122d2aea5
Karesis <yangyifeng23@mails.ucas.ac.cn>
2025-11-15 11:31:30 +0800

    style(strhashmap): move struct defs to header file.

--- include/std/hashmap/strhashmap.h
+++ include/std/hashmap/strhashmap.h
@@ -1,12 +1,12 @@
 /*
  *    Copyright 2025 Karesis
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,8 +19,14 @@
 #include <core/mem/allocer.h>
 #include <stdbool.h>
 
+typedef struct {
+  char *key;
+
+  void *value;
+} Entry;
+
 /**
- * @brief (Opaque) 字符串哈希表
+ * @brief 字符串哈希表
  *
  * 这是一个 "const char* -> void*" 的哈希表。
  * 它使用开放寻址和线性探测。
@@ -29,7 +35,12 @@
  * - 值 (void*) 按原样存储。
  * - 它不拥有 `void*` 值指向的内存。
  */
-typedef struct strhashmap strhashmap_t;
+typedef struct strhashmap {
+  allocer_t *alc;
+  Entry *entries;
+  size_t capacity;
+  size_t count;
+} strhashmap_t;
 
 /**
  * @brief 创建一个新的字符串哈希表。
--- src/std/hashmap/strhashmap.c
+++ src/std/hashmap/strhashmap.c
@@ -1,12 +1,12 @@
 /*
  *    Copyright 2025 Karesis
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,19 +26,6 @@
 #define HASHMAP_MAX_LOAD_FACTOR 0.75
 
 #define TOMBSTONE ((char *)1)
-
-typedef struct {
-  char *key;
-
-  void *value;
-} Entry;
-
-struct strhashmap {
-  allocer_t *alc;
-  Entry *entries;
-  size_t capacity;
-  size_t count;
-};
 
 static uint64_t hash_string(const char *key) {
   uint64_t hash = 5381;
